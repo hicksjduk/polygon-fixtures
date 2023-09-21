29423f710d43c06130b95f6b9a927e49fa4ede66
Jeremy Hicks <jeremy@thehickses.org.uk>
2023-09-21 15:47:39 +0100

    Organise imports

--- src/main/java/polygon/FixtureGenerator.java
+++ src/main/java/polygon/FixtureGenerator.java
@@ -4,7 +4,6 @@ import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.function.BiFunction;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
