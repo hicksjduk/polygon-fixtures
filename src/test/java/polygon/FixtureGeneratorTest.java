57d18f1546ccd22fe2aef8524036fa377fda8ea1
Jeremy Hicks <jeremy@thehickses.org.uk>
2023-09-27 14:54:26 +0100

    Testing

--- src/test/java/polygon/FixtureGeneratorTest.java
+++ src/test/java/polygon/FixtureGeneratorTest.java
@@ -3,8 +3,12 @@ package polygon;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.params.provider.Arguments.*;
 
-import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -27,28 +31,56 @@ class FixtureGeneratorTest
 
     <T> void testRounds(List<T> teams, int games, List<Match<T>> matches)
     {
-        var count = matches.size();
-        assertThat(count % games).isEqualTo(0);
-        var matchesPerRound = count / games;
+        var teamCount = teams.size();
+        var matchCount = matches.size();
+        var roundCount = games * (teamCount - 1 + teamCount % 2);
+        var matchesPerRound = (teamCount - teamCount % 2) / 2;
+        assertThat(matchCount).isEqualTo(roundCount * matchesPerRound);
         var rounds = IntStream
-                .iterate(0, i -> i < count, i -> i + matchesPerRound)
-                .mapToObj(i -> teams.subList(i, i + matchesPerRound))
+                .iterate(0, i -> i < matchCount, i -> i + matchesPerRound)
+                .mapToObj(i -> matches.subList(i, i + matchesPerRound))
+                .toList();
+        var opponentsPerRound = rounds
+                .stream()
+                .map(r -> r
+                        .stream()
+                        .flatMap(m -> Stream
+                                .of(Pair.of(m::home, m::away), Pair.of(m::away, m::home)))
+                        .collect(Collectors.toMap(Pair::first, Pair::second)))
                 .toList();
         teams.forEach(t ->
         {
-            var playedPerRound = rounds
+            assertThat(opponentsPerRound.stream().filter(r -> !r.containsKey(t)).count())
+                    .isEqualTo(games * (teamCount % 2));
+            assertThat(opponentsPerRound
                     .stream()
-                    .map(Collection::stream)
-//                    .map(m -> List.of(m.home(), m.away()))
-            // .mapToLong(r -> r.stream()
-            // .filter(m -> Stream.of(m.home(), m.away()).anyMatch(x.equals(t)))
-            // .count());
-            ;
+                    .map(r -> r.get(t))
+                    .filter(Objects::nonNull)
+                    .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                    .values()).allMatch(c -> c == games);
+            var homeAway = matches
+                    .stream()
+                    .map(m -> m.home().equals(t) ? "H" : m.away().equals(t) ? "A" : "")
+                    .collect(Collectors.joining());
+            assertThat(Pattern.compile("HHH|AAA").matcher(homeAway).results().count()).isEqualTo(0);
+            assertThat(Pattern.compile("HH|AA").matcher(homeAway).results().count())
+                    .isLessThanOrEqualTo(games);
+            assertThat(Pattern.compile("H").matcher(homeAway).results().count())
+                    .isCloseTo(Pattern.compile("A").matcher(homeAway).results().count(),
+                            byLessThan(games * ((teamCount - 1) % 2) + 1L));
         });
     }
 
+    static record Pair<T> (T first, T second)
+    {
+        static <T> Pair<T> of(Supplier<T> first, Supplier<T> second)
+        {
+            return new Pair<>(first.get(), second.get());
+        }
+    };
+
     static Stream<Arguments> testFixtureGenerator()
     {
-        return Stream.of(arguments(10, 1));
+        return Stream.of(arguments(10, 1), arguments(9, 1), arguments(4, 3));
     }
 }
